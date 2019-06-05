b3e109068bd0b82ba17687cf0679a5f5ba9b5d32
Lino Pegoretti <lino.pegoretti@gmail.com>
2019-06-05 19:43:11 -0300

    Add project files.

---
+++ CalculadoraTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DemoTDD
+{
+    class CalculadoraTests
+    {
+        private Calculadora Calculadora = new Calculadora();
+
+        [Test]
+        public void Deve_somar_dois_numeros()
+        {
+            var valorRetornado = Calculadora.Calcular(1, 1, "+");
+            valorRetornado.Should().Be(2);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DemoTDD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DemoTDD")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4004a2f7-25b3-4443-ae43-469f4793e007")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
