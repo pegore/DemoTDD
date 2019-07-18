8dc338ff22e7ffdb315a940f1c70c32636abfc75
Lino Pegoretti <lino.hbsis@gmail.com>
2019-07-18 07:27:37 -0300

    Coding DOJO 17-07-2019

--- Calculadora.cs
+++ Calculadora.cs
@@ -2,15 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DemoTDD
 {
     class Calculadora
     {
-        internal int Calcular(int v1, int v2, string operacao)
+        internal int Calcular(string entrada)
         {
-            return v1 + v2;
+            if (string.IsNullOrEmpty(entrada))
+            {
+                return 0;
+            }
+
+            int inicio = -1, fim = -1;
+
+            inicio = entrada.IndexOf("[");
+            fim = entrada.LastIndexOf("]");
+
+            var delimitadores = entrada.Substring(inicio + 1, fim - inicio).Replace("]", "").Split('[').Where(x => !String.IsNullOrEmpty(x));
+
+            // entrada = entrada.Substring(entrada.IndexOf("\n") + 1);
+
+            entrada = entrada.Substring(fim + 1, entrada.Length - fim - 1);
+
+            foreach (var delimitador in delimitadores)
+            {
+                entrada.Replace(delimitador, ",");
+            }
+
+            foreach (var item in delimitadores)
+            {
+                entrada = entrada.Replace(item, ",");
+            }
+
+            //var retorno = Regex.Replace(entrada, @"[\[%\]\[$\]]+|$+|%+|[\[***\]]+", ",").Replace("//;", "")
+            //                     .Replace(";", ",")
+            //                     .Replace("\n", ",")
+            //                     .Replace(",,", ",")
+            //                     .Split(',')
+            //                     .Where(w => !string.IsNullOrWhiteSpace(w));
+
+
+            var retorno = entrada.Replace("//;", "")
+                                 .Replace(";", ",")
+                                 .Replace("\n", ",")
+                                 .Replace(",,", ",")
+                                 .Split(',')
+                                 .Where(w => !string.IsNullOrWhiteSpace(w));
+
+            var total = 0;
+            foreach (var item in retorno)
+            {
+                var numero = Convert.ToInt16(item);
+                if (numero < 0)
+                {
+                    throw new Exception($"Número negativo não permitido: {item}");
+                }
+
+                if (numero > 0 && numero < 1000)
+                    total += numero;
+            }
+                                 //.Sum(s => Convert.ToInt32(s));
+                        
+            return total;
         }
     }
 }
--- CalculadoraTests.cs
+++ CalculadoraTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using System;
 
 namespace DemoTDD
 {
@@ -8,10 +9,113 @@ namespace DemoTDD
         private Calculadora Calculadora = new Calculadora();
 
         [Test]
-        public void Deve_somar_dois_numeros()
+        [TestCase("0",0)]
+        [TestCase("1",1)]
+        public void Deve_receber_string_retornar_valor_inteiro(string entrada, int resultado)
         {
-            var valorRetornado = Calculadora.Calcular(1, 1, "+");
-            valorRetornado.Should().Be(2);
+            var valorRetornado = Calculadora.Calcular(entrada);
+            valorRetornado.Should().Be(resultado);
+        }
+
+        /*[Test]
+        [TestCase("1,2", 12)]
+        [TestCase("1", 1)]
+        public void ConcatenarValores(string entrada, int resultado)
+        {
+            var valorRetornado = Calculadora.Calcular(entrada);
+            valorRetornado.Should().Be(resultado);
+        }*/
+
+        [Test]
+        [TestCase("1,2", 3)]
+        public void SomarValores(string entrada, int resultado)
+        {
+            var valorRetornado = Calculadora.Calcular(entrada);
+            valorRetornado.Should().Be(resultado);
+        }
+
+        [Test]
+        [TestCase("", 0)]
+        public void RetornarZero(string entrada, int resultado)
+        {
+            var valorRetornado = Calculadora.Calcular(entrada);
+            valorRetornado.Should().Be(resultado);
+        }
+
+        [Test]
+        [TestCase("1,2,2,3,4,1", 13)]
+        public void QuantidadeDeNumerosIndeterminada(string entrada, int resultado)
+        {
+            var valorRetornado = Calculadora.Calcular(entrada);
+            valorRetornado.Should().Be(resultado);
+        }
+
+        [Test]
+        [TestCase("1\n2,3", 6)]
+        public void TratarQuebraLinha(string entrada, int resultado)
+        {
+            var valorRetornado = Calculadora.Calcular(entrada);
+            valorRetornado.Should().Be(resultado);
+        }
+
+
+        [Test]
+        [TestCase("//;\n1;2;3;4", 10)]
+        public void TratarDelimitador(string entrada, int resultado)
+        {
+            var valorRetornado = Calculadora.Calcular(entrada);
+            valorRetornado.Should().Be(resultado);
+        }
+
+        [Test]
+        [TestCase("1,2,-3,4", -3)]
+        public void TratarNumeroNegativo(string entrada, int resultado)
+        {
+            try
+            {
+                var valorRetornado = Calculadora.Calcular(entrada);
+                valorRetornado.Should().NotBe(resultado);
+                
+
+            }
+            catch (Exception e)
+            {
+                e.Message.Should().Be("Número negativo não permitido: " + resultado);
+            }
+        }
+
+
+        [Test]
+        [TestCase("1, 2, 1001, 4", 7)]
+        [TestCase("1001, 1002" , 0)]
+        public void IgnorarMil(string entrada, int resultado)
+        {
+            var valorRetornado = Calculadora.Calcular(entrada);
+            valorRetornado.Should().Be(resultado);
+        }
+
+        [Test]
+        [TestCase("//[***]\n4***1***2", 7)]
+        public void TratarDelimitadorCustomizado(string entrada, int resultado)
+        {
+            var valorRetornado = Calculadora.Calcular(entrada);
+            valorRetornado.Should().Be(resultado);
+        }
+
+        [Test]
+        [TestCase("//[%][$]\n8$2%0", 10)]
+        public void TratarMultiDelimitadores(string entrada, int resultado)
+        {
+            var valorRetornado = Calculadora.Calcular(entrada);
+            valorRetornado.Should().Be(resultado);
+        }
+
+        [Test]
+        [TestCase("//[%%][$$$]\n6$$$2%%7", 15)]
+        public void TratarMultiDelimitadoresMaior(string entrada, int resultado)
+        {
+            var valorRetornado = Calculadora.Calcular(entrada);
+            valorRetornado.Should().Be(resultado);
         }
     }
 }
